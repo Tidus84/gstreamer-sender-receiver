83d1f1abc484fba43c087e289248bdb4a49c5e95
Antonio Rossi <ant.rossi.84@gmail.com>
2020-08-11 10:57:06 +0200

    Update receiver.cpp

--- receiver.cpp
+++ receiver.cpp
@@ -10,17 +10,17 @@ int main()
 	VideoCapture cap("udpsrc port=5000 ! application/x-rtp,media=video,payload=26,clock-rate=90000,encoding-name=JPEG,framerate=30/1 ! rtpjpegdepay ! jpegdec ! videoconvert ! appsink", CAP_GSTREAMER);
 
 	if (!cap.isOpened()) {
-        cerr << "VideoCapture not opened" << endl;
-        exit(-1);
-    }
+		cerr << "VideoCapture not opened" << endl;
+		exit(-1);
+	}
     
-    while (true) {
-        Mat frame;
-        cap.read(frame);        
-        imshow("Receiver window", frame);
-        if (waitKey(1) == 27)
-            break;
-    }
+	while (true) {
+		Mat frame;
+		cap.read(frame);        
+		imshow("Receiver window", frame);
+		if (waitKey(1) == 27)
+			break;
+	}
 
     return 0;
 }
