09e0273753063e04c7348c6f7738c3c587527d82
Antonio Rossi <ant.rossi.84@gmail.com>
2020-08-11 10:55:05 +0200

    Add files via upload

---
+++ receiver.cpp
@@ -0,0 +1,26 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+
+int main()
+{
+	VideoCapture cap("udpsrc port=5000 ! application/x-rtp,media=video,payload=26,clock-rate=90000,encoding-name=JPEG,framerate=30/1 ! rtpjpegdepay ! jpegdec ! videoconvert ! appsink", CAP_GSTREAMER);
+
+	if (!cap.isOpened()) {
+        cerr << "VideoCapture not opened" << endl;
+        exit(-1);
+    }
+    
+    while (true) {
+        Mat frame;
+        cap.read(frame);        
+        imshow("Receiver window", frame);
+        if (waitKey(1) == 27)
+            break;
+    }
+
+    return 0;
+}
---
+++ sender.cpp
@@ -0,0 +1,33 @@
+#include "opencv2/opencv.hpp"
+#include "opencv2/videoio.hpp"
+#include <string>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+
+int main()
+{
+	VideoCapture cap(0, CAP_V4L2);
+
+	if (!cap.isOpened()) {
+		cerr << "VideoCapture not opened" << endl;
+		exit(-1);
+	}
+
+	VideoWriter writer("appsrc ! videoconvert ! video/x-raw,format=YUY2,width=640,height=480,framerate=30/1 ! jpegenc ! rtpjpegpay ! udpsink host=127.0.0.1 port=5000", CAP_GSTREAMER, 0, 30, Size(640, 480), true);
+
+	if (!writer.isOpened()) {
+		cerr << "VideoWriter not opened" << endl;
+		exit(-1);
+	}
+
+	while (true) {
+		Mat frame;
+		cap.read(frame);
+		writer.write(frame);
+	}
+
+	return 0;
+}
